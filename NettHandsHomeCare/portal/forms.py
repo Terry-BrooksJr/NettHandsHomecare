c1b8b88ac74412a6cb468e80893b4506b1d49a6d
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2023-06-24 17:26:23 -0400

    init commit

---
+++ NettHandsHomeCare/portal/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+from portal.models import Employee
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ("username", "email")
+
+class LogInForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
