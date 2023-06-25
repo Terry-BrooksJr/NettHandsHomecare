3d2a3dbffe34c0a29a0d24c029c5f6541de8f533
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2023-06-25 05:23:16 -0500

    (views, templates): Modifies form processing routes, templates and landing

--- NettHandsHomeCare/web/urls.py
+++ NettHandsHomeCare/web/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 import web.views
 
 urlpatterns = [
-    path("", views.index, name='index'),
-    
+    path("", views.index, name="index"),
+    path("client-interest/", views.client_interest, name="client-interest"),
+    path("submission-confirmation/", views.submitted, name='submission-confirmation'),
+
 ]
