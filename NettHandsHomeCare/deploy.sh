9edb3e8690667589ace4d116f2bd2e0b13836076
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2023-07-01 13:28:01 -0500

    Changes paths of Manage.py in deploy script

--- NettHandsHomeCare/deploy.sh
+++ NettHandsHomeCare/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if pip install -r requirements.txt; then
-    if cd NettHandsHomeCare; then
-        if python NettHandsHomeCare/manage.py collectstatic; then
-            if python NettHandsHomeCare/manage.py makemigrations; then
+    # if cd NettHandsHomeCare; then
+        if python manage.py collectstatic; then
+            if python manage.py makemigrations; then
                 if python manage.py migrate; then
                     if ! python manage.py runserver; then
                         exit 1
@@ -11,6 +11,6 @@ if pip install -r requirements.txt; then
                 fi
 
             fi
-        fi
+
     fi
 fi
