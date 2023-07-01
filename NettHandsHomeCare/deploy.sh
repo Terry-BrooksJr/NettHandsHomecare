963bd74abd513c26f7b08d2b90af26fbe0110cb8
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2023-07-01 13:29:54 -0500

    Adds -y to collectstatic

--- NettHandsHomeCare/deploy.sh
+++ NettHandsHomeCare/deploy.sh
@@ -2,7 +2,7 @@
 
 if pip install -r requirements.txt; then
     # if cd NettHandsHomeCare; then
-        if python manage.py collectstatic; then
+        if python manage.py collectstatic -y; then
             if python manage.py makemigrations; then
                 if python manage.py migrate; then
                     if ! python manage.py runserver; then
