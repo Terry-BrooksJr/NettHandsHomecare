5a57dbd661632c4a830e87a9d91f3fff1047511d
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2023-07-01 12:14:29 -0500

    updated deploy script

--- NettHandsHomeCare/deploy.sh
+++ NettHandsHomeCare/deploy.sh
@@ -2,8 +2,15 @@
 
 if pip install -r requirements.txt; then
     if cd NettHandsHomeCare; then
-        if ! python manage.py runserver; then
-            exit 1
+        if python manage.py collectstatic; then
+            if python manage.py makemigrations; then
+                if python manage.py migrate; then
+                    if ! python manage.py runserver; then
+                        exit 1
+                    fi
+                fi
+
+            fi
         fi
     fi
 fi
