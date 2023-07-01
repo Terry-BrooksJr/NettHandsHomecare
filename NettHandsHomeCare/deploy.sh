e31015fb21fa1fbedce74e6da2508599e6e2e4cc
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2023-07-01 13:05:28 -0500

    Modifies Paths in Deploy Script

--- NettHandsHomeCare/deploy.sh
+++ NettHandsHomeCare/deploy.sh
@@ -2,8 +2,8 @@
 
 if pip install -r requirements.txt; then
     if cd NettHandsHomeCare; then
-        if python manage.py collectstatic; then
-            if python manage.py makemigrations; then
+        if python NettHandsHomeCare/manage.py collectstatic; then
+            if python NettHandsHomeCare/manage.py makemigrations; then
                 if python manage.py migrate; then
                     if ! python manage.py runserver; then
                         exit 1
