e7def816c0eeb15d60ee6c5ea203b7a6ae14fa6d
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2023-07-09 21:03:04 -0500

    feat(js) Implements tab filter

--- NettHandsHomeCare/static/js/custom-scripts.js
+++ NettHandsHomeCare/static/js/custom-scripts.js
@@ -1,37 +1,3 @@
-// SECTION - Profile Editabiity Toggle
-
-const editProfileButton = document.getElementById("edit-button")
-const cancelChangesButton = document.getElementById("reset-id-cancel")
-const saveChangeButton = document.getElementById("submit-id-save")
-
-function makeProfileEditable() {
-  const editableFields = document.querySelectorAll('.editable')
-  editableFields.forEach((field) => {
-    field.removeAttribute('readonly')
-  })
-  saveChangeButton.removeAttribute('hidden')
-  cancelChangesButton.removeAttribute('hidden')
-  editProfileButton.hidden = true
-}
-
-function cancelPendingEdits() {
-  window.location.reload();
-}
-
-function makeProfileUnEditable() {
-  const editableFields = document.querySelectorAll('.editable')
-  console.log('Called')
-  editableFields.forEach((field) => {
-    console.log(field)
-    field.disabled = true
-    console.log(`${field} is disabled`)
-  })
-  cancelChangesButton.hidden = true
-  saveChangeButton.hidden = true
-}
-
-
-// !SECTION
 
 // SECTIION - Client Submission Filtering & Pagination
 const $submissions = $(".client-submission")
@@ -149,7 +115,7 @@ $(document).ready(function () {
 function ntfy(cb) {
   alert('Submission Marked as Reviewed')
   console.info('Getting Ready to Reload Page')
-  setTimeout(reloadPage(), 500000);
+  reloadPage()
 };
 function reloadPage() {
   document.location.reload()
